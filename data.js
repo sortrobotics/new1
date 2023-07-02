e191c233d2a62c29178452c49167e93f0d0907f7
sortrobotics <sortrobotics@gmail.com>
2023-07-02 15:25:06 +0530

    commit

---
+++ data.js
@@ -0,0 +1,503 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01-chaturmukh-360-07",
+      "name": "01. Chaturmukh 360 07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0.4113990379700816,
+        "pitch": -0.0049450186706465615,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7176042028234715,
+          "pitch": 0.09036051250406629,
+          "rotation": 0,
+          "target": "1-01-chaturmukh-360-06"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-01-chaturmukh-360-06",
+      "name": "01. Chaturmukh 360 06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -0.10507594476292859,
+        "pitch": -0.004945018670635903,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3191385344802846,
+          "pitch": 0.03672363934320799,
+          "rotation": 4.71238898038469,
+          "target": "3-01-chaturmukh-360-11"
+        },
+        {
+          "yaw": 1.070939464522123,
+          "pitch": 0.1833544038744055,
+          "rotation": 4.71238898038469,
+          "target": "2-01-chaturmukh-360-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-01-chaturmukh-360-05",
+      "name": "01. Chaturmukh 360 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": -0.01959043037953556,
+        "pitch": -0.22499834951337405,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6251342729890794,
+          "pitch": -0.11179435741075494,
+          "rotation": 0.7853981633974483,
+          "target": "1-01-chaturmukh-360-06"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-01-chaturmukh-360-11",
+      "name": "01. Chaturmukh 360 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6539487201210807,
+          "pitch": 0.13596173500266673,
+          "rotation": 0,
+          "target": "4-01-chaturmukh-360-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-01-chaturmukh-360-03",
+      "name": "01. Chaturmukh 360 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6490184946901163,
+          "pitch": 0.21008007120024175,
+          "rotation": 4.71238898038469,
+          "target": "5-01-chaturmukh-360-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-01-chaturmukh-360-02",
+      "name": "01. Chaturmukh 360 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0706039894877986,
+          "pitch": 0.15697651852769035,
+          "rotation": 4.71238898038469,
+          "target": "6-01-chaturmukh-360-04"
+        },
+        {
+          "yaw": 0.4135891621762582,
+          "pitch": 0.013485665177157102,
+          "rotation": 10.995574287564278,
+          "target": "8-01-chaturmukh-360-08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-01-chaturmukh-360-04",
+      "name": "01. Chaturmukh 360 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7253884054303388,
+          "pitch": 0.041852831550679426,
+          "rotation": 4.71238898038469,
+          "target": "7-01-chaturmukh-360-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-01-chaturmukh-360-01",
+      "name": "01. Chaturmukh 360 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5323910664856264,
+          "pitch": 0.04058522406529441,
+          "rotation": 4.71238898038469,
+          "target": "4-01-chaturmukh-360-03"
+        },
+        {
+          "yaw": -0.27558875236345237,
+          "pitch": -0.03400100615892221,
+          "rotation": 1.5707963267948966,
+          "target": "10-01-chaturmukh-360-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-01-chaturmukh-360-08",
+      "name": "01. Chaturmukh 360 08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.019590430379519574,
+        "pitch": 0.09148284540653151,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5974840777988195,
+          "pitch": 0.14747687566859113,
+          "rotation": 3.9269908169872414,
+          "target": "9-01-chaturmukh-360-09"
+        },
+        {
+          "yaw": -0.8951713314425547,
+          "pitch": 0.17609554576929654,
+          "rotation": 4.71238898038469,
+          "target": "4-01-chaturmukh-360-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-01-chaturmukh-360-09",
+      "name": "01. Chaturmukh 360 09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.017809482163215762,
+        "pitch": 0.002471577527325408,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0774754837996188,
+          "pitch": 0.19442493538943673,
+          "rotation": 4.71238898038469,
+          "target": "10-01-chaturmukh-360-10"
+        },
+        {
+          "yaw": -0.26603861927657313,
+          "pitch": 0.14452064764768835,
+          "rotation": 2.356194490192345,
+          "target": "8-01-chaturmukh-360-08"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-01-chaturmukh-360-10",
+      "name": "01. Chaturmukh 360 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -0.26892318066442655,
+        "pitch": 0.608237296486692,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.058068569374693624,
+          "pitch": 0.3278078109680056,
+          "rotation": 5.497787143782138,
+          "target": "9-01-chaturmukh-360-09"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "चतुर्मुख",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
